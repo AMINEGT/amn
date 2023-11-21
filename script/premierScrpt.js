fe941fa854e8e59fcddf8f18b62f2a2224b503ec
Amine59370 <bgamine89@gmail.com>
2023-11-21 16:44:31 +0100

    javascript

---
+++ script/premierScrpt.js
@@ -0,0 +1,29 @@
+//console.log("Hello worl!")
+
+/*let nomUtilisateur*/
+/*let ageu=45
+let nomUtilisateur
+let ageuUtilisateur
+nomUtilisateur = YNZ
+ageuUtilisateur = 56
+nomUtilisateur = prompt("comment vous appelez-vous?*")
+ageuUtilisateur = prompt("Quelest votre age?")*/
+/*let a = "YNZ";
+let b = "au top";
+console.log(a+""+b)*/
+/*alet(`bonjour ${nomUtilisateur}, vous avez ${ageUtilisateur}ans!`)*/
+
+/* Premier script complet*/
+whille (userNumber != randomTraget,)
+count= count++
+let randomTraget = Math.floor(Math.random() * 100) +1
+console.log(randomTraget)
+let userNumber = prompt("Devinez un nombre entre 1 et 100")
+
+if (isNaN(userNumber > randomTraget) ) {
+alert("C'est trop grand")
+} else if (userNumber < randomTraget){
+    alert("C'est trop petit ")
+} else{
+alert("Bravo t'as trouvé")
+}
